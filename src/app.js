67c60911569d07b4c1ff8c4649c88688f9d4fdbc
emersonmadrid <emersonmadrid@gmail.com>
2025-11-21 12:01:41 -0500

    update precios

--- src/app.js
+++ src/app.js
@@ -154,7 +154,7 @@ function quickAnswers(text, conversationContext = null) {
 
   if (/(precio|cu[aá]nto cuesta|cuanto|tarifa|costo)/.test(t) &&
     !/(agendar|cita|reservar)/.test(t)) {
-    return "Nuestros precios:\n• Terapia psicológica: S/ 140 (50 min, online)\n• Consulta psiquiátrica: S/ 200 (online)\n¿Te gustaría agendar una cita?";
+    return "Nuestros precios:\n• Terapia psicológica: S/ 80 (50 min, Presencial/Online)\n• Consulta psiquiátrica: S/ 139 (online)\n¿Te gustaría agendar una cita?";
   }
 
   if (/(horario|atienden|atenci[oó]n|abren|disponibilidad)/.test(t) &&
